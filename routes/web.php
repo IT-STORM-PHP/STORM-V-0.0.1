92c1b07a010e0fbb9e3354a364305a394f732c1c
Godwill05 <godwilloussou@gmail.com>
2025-02-15 04:05:24 +0100

    revu de code

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,3 @@
 
     Route::get('/', [HomeControllers::class, 'index']);
 
-use App\Controllers\CategoriesController;
-Route::get('/categories', [CategoriesController::class, 'index']);
-Route::get('/categories/create', [CategoriesController::class, 'create']);
-Route::post('/categories/store', [CategoriesController::class, 'store']);
-Route::get('/categories/edit/{id}', [CategoriesController::class, 'edit']);
-Route::post('/categories/update/{id}', [CategoriesController::class, 'update']);
-Route::post('/categories/delete/{id}', [CategoriesController::class, 'destroy']);
-Route::get('/categories/show/{id}', [CategoriesController::class, 'show']);
