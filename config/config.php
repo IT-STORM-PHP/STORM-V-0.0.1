f455460bb2ab3b4cfc0b0129186431adf08aab2a
Godwill05 <godwilloussou@gmail.com>
2025-02-16 03:49:54 +0100

    will

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
     putenv('ENV=test');
     return [
-        'db_host' => 'mysql-godwill05.alwaysdata.net',
+        'db_host' => '',
         'db_port' => '3306',
-        'db_name' => 'godwill05_storm',
-        'db_user' => 'godwill05',
-        'db_pass' => 'Godwill.2005',
+        'db_name' => '',
+        'db_user' => '',
+        'db_pass' => '',
         'errors_route_message' => "Impossible d'accéder à cette URL, car elle n'est pas définie. Définissez-la avec :",
     ];
 ?>
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,3 @@
     use App\Controllers\HomeControllers;
     
     Route::get('/', [HomeControllers::class, 'index']);
-
-use App\Controllers\ArticlesController;
-Route::get('/articles', [ArticlesController::class, 'index']);
-Route::get('/articles/create', [ArticlesController::class, 'create']);
-Route::get('/articles/show/{id}', [ArticlesController::class, 'show']);
-Route::post('/articles/store', [ArticlesController::class, 'store']);
-Route::get('/articles/edit/{id}', [ArticlesController::class, 'edit']);
-Route::post('/articles/update/{id}', [ArticlesController::class, 'update']);
-Route::post('/articles/delete/{id}', [ArticlesController::class, 'destroy']);
-
-use App\Controllers\Login\LoginController;
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [LoginController::class, 'register']);
