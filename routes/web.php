04967f63645a755de8159a704ab02d97c2b5ebeb
Godwill05 <godwilloussou@gmail.com>
2025-02-17 06:35:36 +0100

    remis a neuf

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,3 @@
     use App\Controllers\HomeControllers;
     
     Route::get('/', [HomeControllers::class, 'index']);
-use App\Controllers\Login\LoginController;
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [LoginController::class, 'register']);
-Route::get('/register/page', [LoginController::class, 'registerpage']);
-Route::get('/login/page', [LoginController::class, 'loginpage']);
