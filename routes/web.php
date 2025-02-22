6abeca099c148ad53d46e4e7379b8c18734904fd
Godwill05 <godwilloussou@gmail.com>
2025-02-22 01:58:40 +0100

    revue de code

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use App\Controllers\HomeControllers;
 
 Route::get('/', [HomeControllers::class, 'index']);
 
-
-
-use App\Controllers\LivresController;
-Route::get('/livres', [LivresController::class, 'index']);
-Route::get('/livres/create', [LivresController::class, 'create']);
-Route::get('/livres/show/{id}', [LivresController::class, 'show']);
-Route::post('/livres/store', [LivresController::class, 'store']);
-Route::get('/livres/edit/{id}', [LivresController::class, 'edit']);
-Route::post('/livres/update/{id}', [LivresController::class, 'update']);
-Route::post('/livres/delete/{id}', [LivresController::class, 'destroy']);
-
-
